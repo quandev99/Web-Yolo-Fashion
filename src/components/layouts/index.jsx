3a0a92d0422b6d44074049db84ec11426dc20b49
quandev99 <nguyenvietquan2605@gmail.com>
2023-12-28 18:13:21 +0700

    update

--- src/components/layouts/index.jsx
+++ src/components/layouts/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Header from "../header";
-import Footer from "../footer";
 import MyRoutes from "../../routes/Routes"; // Adjust the import path accordingly
 import ProductDetailModal from "../portal/productDetailModal";
+import Footer from "../footer";
 
 const Layout = () => {
   return (
